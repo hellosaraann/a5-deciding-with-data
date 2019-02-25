91a3b7abb53ec370f24420e8747b5aa396db1c4a
hellosaraann <srichardson@mcad.edu>
2019-02-24 21:39:16 -0600

    Added donate button authorAge feature script

--- script.js
+++ script.js
@@ -20,31 +20,53 @@ var churchillSpeech = {
     speechesArray = [churchillSpeech, ghandiSpeech, demosthenesSpeech];
 
 //greetings & setting of user names
-var userName = window.prompt("What is your name?");
+var userName = window.prompt( "What is your name?" );
 
-if (typeof userName == "string" && userName !== "") {
-  console.log("Hi " + userName + "!")
+if ( typeof userName == "string" && userName !== "" ) {
+  console.log( "Hi " + userName + "!" )
 } else {
-  console.log("Okay, I'll just call you User")
+  console.log( "Okay, I'll just call you User" )
 };
 
-if (userName == "") {
+if ( userName == "" ) {
   var userName = "User";
-}
+};
 
+//authorAge interactive elements
+document.getElementById( 'BtnDonate' ).addEventListener( 'click', function() {
 
-document.getElementById('BtnDonate').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Donate" button.
-});
+  var faveAuthor = window.prompt( "Which author here is your favorite: Churchill, Ghandi or Demosthenes?" );
 
-document.getElementById('BtnChurchill').addEventListener('click', function(){
+  switch( faveAuthor ) {
+
+    case "Churchill" :
+      console.log( churchillSpeech.author + " was " + churchillSpeech.authorAge + " during this speech.");
+      break;
+
+      case "Ghandi" :
+        console.log( ghandiSpeech.author + " was " + ghandiSpeech.authorAge + " during this speech." );
+        break;
+
+      case "Demosthenes" :
+        console.log( demosthenesSpeech.author + " was " + demosthenesSpeech.authorAge + " during this speech." );
+        break;
+
+      default :
+        console.log( "Incorrect entry. Please click donate to try again, and enter one name only:  Churchill, Ghandi or Demosthenes.");
+        break;
+
+    };
+
+} );
+
+document.getElementById( 'BtnChurchill' ).addEventListener( 'click', function() {
   //Code in here executes when the user clicks the "Churchill" button.
-});
+} );
 
-document.getElementById('BtnGhandi').addEventListener('click', function(){
+document.getElementById( 'BtnGhandi' ).addEventListener( 'click', function() {
   //Code in here executes when the user clicks the "Ghandi" button.
-});
+} );
 
-document.getElementById('BtnDemosthenes').addEventListener('click', function(){
+document.getElementById( 'BtnDemosthenes' ).addEventListener( 'click', function() {
   //Code in here executes when the user clicks the "Demosthenes" button.
-});
+} );
